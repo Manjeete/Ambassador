d01a63ada831ce4164e53afeceadc18abcd5cbdf
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-31 22:50:29 +0530

    error

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,12 @@
 from typing import ValuesView
+
+from django.core import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import LinkSerializer
-from core.models import Link
+from core.models import Link,Order, OrderItem, Product
+import decimal
 
 
 class LinkAPIView(APIView):
@@ -17,4 +20,39 @@ class LinkAPIView(APIView):
 class OrderAPIView(APIView):
 
     def post(self,request):
-        pass
+        data=request.data
+        link=Link.objects.filter(code=data['code']).first()
+
+        if not link:
+            raise exceptions.APIException('Invalid code!')
+
+        order=Order()
+        order.user_id=link.user.id
+        order.ambassador_email = link.user.email
+        order.first_name = data['first_name']
+        order.last_name = data['last_name']
+        order.email = data['email']
+        order.address = data['address']
+        order.country = data['country']
+        order.city = data['city']
+        order.zip = data['zip']
+        order.save()
+
+        for item in data['prodcuts']:
+            product = Product.objects.filter(pk=item['product_id']).first()
+            quantity = decimal.Decimal(item['quantity'])
+
+            order_item = OrderItem()
+            order_item.order = order
+            order_item.product_title = product.title
+            order_item.price = product.price 
+            order_item.quantity = quantity
+            order_item.ambassador_revenue = decimal.Decimal(0.1)*product.price*quantity
+            order_item.admin_revenue = decimal.Decimal(0.9)*product.price*quantity
+            order_item.save()
+
+        return Response({
+            "message":"success"
+        })
+
+
